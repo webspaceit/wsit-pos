4a153937b12608892919e893995cd48a0a8511e1
webspaceit <webspaceit@gmail.com>
2026-07-22 18:23:08 +0600

    Fix MySQL key length error for utf8mb4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -31,6 +32,8 @@ class AppServiceProvider extends ServiceProvider
      */
     protected function configureDefaults(): void
     {
+        Schema::defaultStringLength(191);
+
         Date::use(CarbonImmutable::class);
 
         DB::prohibitDestructiveCommands(
