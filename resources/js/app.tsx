52400c0a1735b6436b82ff1e21f4228657b55a35
webspaceit <webspaceit@gmail.com>
2026-07-23 19:59:28 +0600

    fix: remove double AppLayout wrapping for non-settings pages

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -19,7 +19,7 @@ createInertiaApp({
             case name.startsWith('settings/'):
                 return [AppLayout, SettingsLayout];
             default:
-                return AppLayout;
+                return null;
         }
     },
     strictMode: true,
